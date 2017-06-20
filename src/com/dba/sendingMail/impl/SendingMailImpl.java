468887aa1e69cf097ab218ba3adf9657f18eeca3
Leo Zhao <leo.zhao@sap.com>
2017-06-20 14:19:28 +0800

    exclude lib

---
+++ src/com/dba/sendingMail/impl/SendingMailImpl.java
@@ -0,0 +1,17 @@
+package com.dba.sendingMail.impl;
+
+import java.util.List;
+import com.dba.data.Customer;
+import com.dba.sendingMail.SendingMail;
+
+public class SendingMailImpl implements SendingMail{
+	
+	
+	public SendingMailImpl() {
+		
+	}
+	
+	public void sendingMailtoCustomers(List<Customer> customers){
+		
+	}
+}
